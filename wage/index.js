e787a7f2e47efc1adf2c6ba61eeb64cee8eb0a8c
redgun524 <pumpapapumpa@gmail.com>
2020-04-07 11:43:59 +0900

    시급 반환 함수 추가

--- wage/index.js
+++ wage/index.js
@@ -1,19 +1,42 @@
 const { isValidRank } = require('../lib/validation')
 
-const hourlyObj = {
-  price: {
-    A: 20000,
-    B: 17000,
-    C: 14000,
+const HOURLY_WAGE_MANUAL = [
+  {
+    A: 12000,
+    B: 10000,
+    C: 8000,
   },
-  wage: {
+  {
+    A: 12500,
+    B: 10500,
+    C: 8500,
+  },
+  {
     A: 14000,
     B: 12000,
     C: 10000,
-  }
-}
+  },
+]
 
 const SIBLING_HOURLY_WAGE = 3000
+
+const getHourlyWage = (type, rank, date) => {
+  let index = 2
+  if (moment(date).isBefore('2019-01-01')) index = 0
+  else if (moment(date).isBefore('2019-10-01')) index = 1
+
+  const index = moment(date).isSameOrAfter('2019-10-01') ? 2 : moment(date).isSameOrBefore('2019-01-01') ? 1 : 0
+  let hourlyWage = HOURLY_WAGE_MANUAL[index][rank]
+  if (type === 'S') {
+    if (index === 0) {
+      hourlyWage = hourlyWage * 1.2
+    } else if (index === 1) {
+      hourlyWage += 1000
+    }
+  }
+
+  return hourlyWage
+}
 
 /**
  * 화폐 단위 문자열로 변환
@@ -32,18 +55,33 @@ const toCurrency = price => {
  * @param {number} sibling 형제 추가 옵션
  * @returns {number}
  */
-const getWageForOnePlay = (rank, hour, sibling) => {
+const getWageForOnePlay = (type, rank, hour, date, sibling) => {
   if (isValidRank(rank)
   && typeof hour === 'number'
   && typeof sibling === 'number') {
-    const type = code.charAt(0)
-    const rank = code.charAt(3)
-    const wagePerHour =
-      getHourlyWage(type, rank, isOld) + sibling * SIBLING_HOURLY_WAGE
-    return wagePerHour * hour
+    const hourlyWage =
+      getHourlyWage(type, rank, date) + sibling * SIBLING_HOURLY_WAGE
+    return hourlyWage * hour
   }
   return 0
 }
 
-exports.toCurrency = toCurrency
-exports.getWageForOnePlay = getWageForOnePlay
+const wage = {
+  toCurrency,
+  getWageForOnePlay,
+};
+
+(function(root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD
+    define(['wage'], factory)
+  } else if (typeof module === 'object' && module.exports) {
+    // CommonJS
+    module.exports = factory(wage)
+  } else {
+    // browser
+    root.isDev = factory()
+  }
+})(this, function() {
+  return wage
+})
